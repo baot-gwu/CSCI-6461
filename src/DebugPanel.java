1f3d55e38909ba2633567044d4d160e8dd87b3ef
baot <baot@gwu.edu>
2019-09-10 02:49:29 -0400

    UI Design Draft

---
+++ src/DebugPanel.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class DebugPanel extends JFrame{
+    private static final int WIDTH = 550;
+    private static final int HEIGHT = 720;
+    private JPanel MainForm;
+    private JPanel Controller;
+    private JButton autoRunButton;
+    private JButton singleRunButton;
+    private JButton loadButton;
+    private JButton startButton;
+    private JButton restartButton;
+    private JButton program1Button;
+    private JButton program2Button;
+    private JButton floatingTestButton;
+    private JButton vectorTestButton;
+    private JButton IPLButton;
+    private JTextField R0_textField;
+    private JTextField R1_textField;
+    private JTextField R2_textField;
+    private JPanel Memory;
+    private JPanel Indicators;
+    private JPanel Index;
+    private JPanel Registers;
+    private JTextField IX1_textField;
+    private JTextField IX2_textField;
+    private JTextField IX3_textField;
+    private JLabel IX1_label;
+    private JLabel IX2_label;
+    private JLabel IX3_label;
+    private JLabel R0_label;
+    private JLabel R1_label;
+    private JLabel R2_label;
+    private JLabel R3_label;
+    private JTextField R3_textField;
+    private JPanel MemoryRegister;
+    private JPanel BasicIndicators;
+    private JTextField MAR_textField;
+    private JTextField MBR_textField;
+    private JLabel MAR_label;
+    private JLabel MBR_label;
+    private JTextField PC_textField;
+    private JTextField IR_textField;
+    private JTextField CC_textField3;
+    private JLabel PC_label;
+    private JLabel IR_label;
+    private JLabel CC_label;
+    private JList MemoryList;
+    private JScrollPane MemoryLIstScroll;
+    private JLabel R0_value;
+    private JLabel R1_value;
+    private JLabel R2_value;
+    private JLabel R3_value;
+    private JLabel IX1_value;
+    private JLabel IX2_value;
+    private JLabel IX3_value;
+    private JLabel MAR_value;
+    private JLabel PC_value;
+    private JLabel CC_value;
+
+    public DebugPanel () {
+        super("Machine Simulator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        setSize(WIDTH, HEIGHT);
+        getContentPane().add(MainForm);
+        setVisible(true);
+
+        MemoryList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getClickCount() == 2){
+                    JDialog.getWindows();
+                }
+            }
+        });
+    }
+}
